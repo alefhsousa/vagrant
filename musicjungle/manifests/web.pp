be52400b39dfd1f297b9014c3270069f2c6d66bf
Alefh Sousa <alefh.sousa@gmail.com>
2015-12-06 18:01:04 -0200

    Versao inicial do vagrant criando uma máquina ubuntu x86. Utilizando o puppet como provisionamento, instalando os pacotes: Java 7, curl e tomcat7

---
+++ musicjungle/manifests/web.pp
@@ -0,0 +1,12 @@
+exec { "apt-update":
+  command => "/usr/bin/apt-get update"
+}
+
+package { ["openjdk-7-jre", "tomcat7"]:
+  ensure => installed,
+  require => Exec["apt-update"]
+}
+
+package {["curl"]:
+  ensure => installed,
+}
